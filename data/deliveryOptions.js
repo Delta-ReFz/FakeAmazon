feec9d51cf2805762b8ab6eb0a893cb09612f18d
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-24 20:53:56 -0500

    Save data & generate HTML for order summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -14,3 +14,16 @@ const deliveryOptions = [{
     priceCents: 999
 }
 ];
+
+function getDeliveryOption(deliveryOptionId) {
+
+    let deliveryOption;
+
+    deliveryOptions.forEach((option) => {
+      if (option.id === deliveryOptionId) {
+        deliveryOption = option;
+      }
+    });
+
+    return deliveryOption || deliveryOptions[0];
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -11,24 +11,11 @@ function renderOrderSummary() {
 
     const productId = cartItem.productId;
 
-    let matchingProduct;
-
-    products.forEach((product) => {
-
-      if (product.id === productId) {
-        matchingProduct = product;
-      };
-    });
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
@@ -130,3 +117,63 @@ function renderOrderSummary() {
 };
 
 renderOrderSummary();
+
+function renderPaymentSummary() {
+
+  let productPriceCents = 0;
+  let shippingPriceCents = 0;
+
+  cart.forEach((cartItem) => {
+   const product = getProduct(cartItem.productId);
+   productPriceCents += product.priceCents * cartItem.quantity;
+
+   const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
+   shippingPriceCents += deliveryOption.priceCents;
+  });
+
+  const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
+  const taxCents = totalBeforeTaxCents * 0.1;
+  const totalCents = totalBeforeTaxCents + taxCents;
+
+  const paymentSummaryHTML = `
+  
+  <div class="payment-summary-title">
+          Order Summary
+        </div>
+
+        <div class="payment-summary-row">
+          <div>Items (3):</div>
+          <div class="payment-summary-money">$${(productPriceCents/ 100).toFixed(2)}</div>
+        </div>
+
+        <div class="payment-summary-row">
+          <div>Shipping &amp; handling:</div>
+          <div class="payment-summary-money">$${(shippingPriceCents/ 100).toFixed(2)}</div>
+        </div>
+
+        <div class="payment-summary-row subtotal-row">
+          <div>Total before tax:</div>
+          <div class="payment-summary-money">$${(totalBeforeTaxCents/ 100).toFixed(2)}</div>
+        </div>
+
+        <div class="payment-summary-row">
+          <div>Estimated tax (10%):</div>
+          <div class="payment-summary-money">$${(taxCents/ 100).toFixed(2)}</div>
+        </div>
+
+        <div class="payment-summary-row total-row">
+          <div>Order total:</div>
+          <div class="payment-summary-money">$${(totalCents/ 100).toFixed(2)}</div>
+        </div>
+
+        <button class="place-order-button button-primary">
+          Place your order
+        </button>
+
+  `;
+
+  document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+};
+
+renderPaymentSummary();
